0d68209bcbab3d67e6f048506cddef7d6f240784
tbn22 <tbn22@scarletmail.rutgers.edu>
2025-12-06 00:25:26 -0500

    signal setup

---
+++ nimd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include "network.h"
